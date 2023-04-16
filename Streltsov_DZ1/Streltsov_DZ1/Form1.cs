c2b784077ae00865422a9e4d5cb15c94d3814753
zeleboba2281337 <115745043+zeleboba2281337@users.noreply.github.com>
2023-04-16 17:24:56 +0300

    Add files via upload

---
+++ Streltsov_DZ1/Streltsov_DZ1/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Streltsov_DZ1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = Calculate(long.Parse(textBox1.Text)).ToString();
+        }
+
+        private long Calculate(long input)
+        {
+            long summ = 0;
+            for (long i = 0; i <= input; i++)
+            {
+                summ += (long)Math.Sqrt(i);
+            }
+            return summ;
+        }
+    }
+}
